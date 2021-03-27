f82014cfdb30e44bcfe0f28ef2327e551e6341c6
Crazy-Newbie <55473294+Crazy-Newbie@users.noreply.github.com>
2021-03-27 18:31:22 +0700

    Add files via upload

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -1,1 +1,25 @@
+#!/bin/bash
 
+modif=$(grep "modified" syslog.log | wc -l)
+echo "The ticket was modified while updating," $modif > error_message.txt
+deny=$(grep "denied" syslog.log | wc -l)
+echo "Permission denied while closing ticket," $deny >> error_message.txt
+tried=$(grep "Tried" syslog.log | wc -l)
+echo "Tried to add information to closed ticket," $tried >> error_message.txt
+timeout=$(grep "Timeout" syslog.log | wc -l)
+echo "Timeout while retrieving information," $timeout >> error_message.txt
+notexist=$(grep "exist" syslog.log | wc -l)
+echo "Ticket doesn't exist," $notexist >> error_message.txt
+connection=$(grep "Connection" syslog.log | wc -l)
+echo "Connection to DB failed," $connection >> error_message.txt
+
+sort -t, -nr -k2 error_message.txt | sed '1i\Count,Error' > error_message.csv
+
+echo "Username,INFO,ERROR" > user_statistic.csv
+cut -d "(" -f2 syslog.log | cut -d ")" -f1 | sort | uniq |
+while read line;
+do
+	err=$(grep -o "ERROR.*($line)" syslog.log | wc -l)
+	inf=$(grep -o "INFO.*($line)" syslog.log | wc -l)
+	echo -e "$line,$inf,$err"
+done >> user_statistic.csv
